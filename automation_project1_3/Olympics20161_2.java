06f9236d264e8e59cc433b5518f8a930d8910fba
mozka002 <mozka002@192.168.13.218>
2018-07-05 18:40:50 -0400

    new additions

--- automation_project1_3/Olympics20161_2.java
+++ automation_project1_3/Olympics20161_2.java
@@ -15,6 +15,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -35,6 +36,11 @@ public class Olympics20161_2 {
 	@BeforeMethod
 	public void start() {
 
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
 	}
 
 	public static String[][] getMedalTable(WebDriver driver) throws InterruptedException {
